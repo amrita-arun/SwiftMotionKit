2334b784d08a0f467057c8ec1cf76b8109f8386b
amrita-arun <amrita.arun2013@gmail.com>
2026-03-09 23:05:42 -0700

    Initial Swift package setup

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftMotionKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftMotionKit",
+            targets: ["SwiftMotionKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftMotionKit"),
+        .testTarget(
+            name: "SwiftMotionKitTests",
+            dependencies: ["SwiftMotionKit"]
+        ),
+    ]
+)
